c19027cae6ca2ffea3a720e000beae6ea782c5ba
junekyu02 <baekjunekyu02@gmail.com>
2025-08-11 05:11:38 +0900

    ㅇㅇ

--- app/src/main/java/site/weshare/android/presentation/chat/ChatScreen.kt
+++ app/src/main/java/site/weshare/android/presentation/chat/ChatScreen.kt
@@ -1,7 +1,39 @@
 package site.weshare.android.presentation.chat
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ChatScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White), // 흰 배경
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "아직 채팅방이 존재하지 않습니다!",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.Gray
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ChatScreenPreview() {
+    MaterialTheme {
+        ChatScreen()
+    }
 }
